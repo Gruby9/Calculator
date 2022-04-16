a5cdca85c147b73cf32782556bca3eb3f09a7078
Piotr <kozlowski.piotr3@gmail.com>
2022-04-16 19:19:31 +0200

    added functionality to number and top buttons

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const numberButton = document.querySelector('.btn')
+const numberButtons = document.querySelectorAll('.btn')
 const screen = document.querySelector('.screen')
+const secondLine = document.querySelector('.second.line')
+const clearButton = document.querySelector('.button.clear')
+const deleteButton = document.querySelector('.button.delete')
 
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        secondLine.textContent += button.textContent;
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    secondLine.textContent = '';
+});
+
+deleteButton.addEventListener('click', () => {
+    const index = (secondLine.textContent.length) - 1
+    secondLine.textContent = secondLine.textContent.slice(0, index)
+})
