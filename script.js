776ba6277eb17a978603d64c6a8e58f4bc4b6f10
Piotr <kozlowski.piotr3@gmail.com>
2022-04-16 19:56:25 +0200

    addes deletebutton, plus button and equal button (result in console)

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const numberButtons = document.querySelectorAll('.btn')
 const screen = document.querySelector('.screen')
+const firstLine = document.querySelector('.first.line')
 const secondLine = document.querySelector('.second.line')
 const clearButton = document.querySelector('.button.clear')
 const deleteButton = document.querySelector('.button.delete')
+const plusButton = document.querySelector('.operation.plus')
+const equalsButton = document.querySelector('.operation.equals')
 
 
 numberButtons.forEach((button) => {
@@ -16,6 +19,17 @@ clearButton.addEventListener('click', () => {
 });
 
 deleteButton.addEventListener('click', () => {
-    const index = (secondLine.textContent.length) - 1
-    secondLine.textContent = secondLine.textContent.slice(0, index)
-})
+    const index = (secondLine.textContent.length) - 1;
+    secondLine.textContent = secondLine.textContent.slice(0, index);
+});
+
+plusButton.addEventListener('click', () => {
+    firstLine.textContent += secondLine.textContent + '+';
+    secondLine.textContent = '';
+});
+
+equalsButton.addEventListener('click', () => {
+    const overalInput = firstLine.textContent + secondLine.textContent;
+    const result = Function('return' + ' ' + overalInput)();
+    console.log(result)
+});
