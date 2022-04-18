60b281d8237a8bcdea0e158a1b862f9c66ae9e1f
Piotr <kozlowski.piotr3@gmail.com>
2022-04-18 15:44:44 +0200

    functionality complete, style complete

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const numberButtons = document.querySelectorAll('.btn')
-const screen = document.querySelector('.screen')
-const firstLine = document.querySelector('.first.line')
-const secondLine = document.querySelector('.second.line')
-const clearButton = document.querySelector('.button.clear')
-const deleteButton = document.querySelector('.button.delete')
-const plusButton = document.querySelector('.operation.plus')
-const equalsButton = document.querySelector('.operation.equals')
+const numberButtons = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+const firstLine = document.querySelector('.first.line');
+const secondLine = document.querySelector('.second.line');
+const clearButton = document.querySelector('.button.clear');
+const deleteButton = document.querySelector('.button.delete');
+const equalsButton = document.querySelector('.operation.equals');
+const plusButton = document.querySelector('.operation.plus');
+const divideButton = document.querySelector('.operation.divide');
+const timesButton = document.querySelector('.operation.times');
+const minusButton = document.querySelector('.operation.minus');
+//const pointButton = document.querySelector('.operation.point')
 
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        secondLine.textContent += button.textContent;
+    secondLine.textContent += button.textContent;
     });
 });
 
 clearButton.addEventListener('click', () => {
     secondLine.textContent = '';
+    firstLine.textContent = '';
 });
 
 deleteButton.addEventListener('click', () => {
@@ -24,12 +29,74 @@ deleteButton.addEventListener('click', () => {
 });
 
 plusButton.addEventListener('click', () => {
-    firstLine.textContent += secondLine.textContent + '+';
-    secondLine.textContent = '';
+    const index = (firstLine.textContent.length) - 1
+    if (firstLine.textContent.includes('=') === false) { 
+        if (firstLine.textContent.charAt(index) != '+' &&
+        firstLine.textContent.charAt(index) != '/' &&
+        firstLine.textContent.charAt(index) != '*' &&
+        firstLine.textContent.charAt(index) != '-') {
+            firstLine.textContent += secondLine.textContent + '+';
+            secondLine.textContent = '';
+        }
+    } else {
+        firstLine.textContent = secondLine.textContent + '+';
+        secondLine.textContent = '';
+    }
 });
 
 equalsButton.addEventListener('click', () => {
-    const overalInput = firstLine.textContent + secondLine.textContent;
-    const result = Function('return' + ' ' + overalInput)();
-    console.log(result)
+    if(firstLine.textContent.includes('=') === false) {
+        firstLine.textContent += secondLine.textContent;
+        const result = Function('return' + ' ' + firstLine.textContent)();
+        firstLine.textContent += '=';
+        secondLine.textContent = result;
+    }
+});
+
+divideButton.addEventListener('click', () => {
+    const index = (firstLine.textContent.length) - 1
+    if (firstLine.textContent.includes('=') === false) { 
+        if (firstLine.textContent.charAt(index) != '+' &&
+        firstLine.textContent.charAt(index) != '/' &&
+        firstLine.textContent.charAt(index) != '*' &&
+        firstLine.textContent.charAt(index) != '-') {
+            firstLine.textContent += secondLine.textContent + '/';
+            secondLine.textContent = '';
+        }
+    } else {
+        firstLine.textContent = secondLine.textContent + '/';
+        secondLine.textContent = '';
+    }
+});
+
+timesButton.addEventListener('click', () => {
+    const index = (firstLine.textContent.length) - 1
+    if (firstLine.textContent.includes('=') === false) { 
+        if (firstLine.textContent.charAt(index) != '+' &&
+        firstLine.textContent.charAt(index) != '/' &&
+        firstLine.textContent.charAt(index) != '*' &&
+        firstLine.textContent.charAt(index) != '-') {
+            firstLine.textContent += secondLine.textContent + '*';
+            secondLine.textContent = '';
+        }
+    } else {
+        firstLine.textContent = secondLine.textContent + '*';
+        secondLine.textContent = '';
+    }
+});
+
+minusButton.addEventListener('click', () => {
+    const index = (firstLine.textContent.length) - 1
+    if (firstLine.textContent.includes('=') === false) { 
+        if (firstLine.textContent.charAt(index) != '+' &&
+        firstLine.textContent.charAt(index) != '/' &&
+        firstLine.textContent.charAt(index) != '*' &&
+        firstLine.textContent.charAt(index) != '-') {
+            firstLine.textContent += secondLine.textContent + '-';
+            secondLine.textContent = '';
+        }
+    } else {
+        firstLine.textContent = secondLine.textContent + '-';
+        secondLine.textContent = '';
+    }
 });
